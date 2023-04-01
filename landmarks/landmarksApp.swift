84327b77bc910d231466f5bd62f6c45b9cd4020b
Andrew J Fei <andrewjfei@gmail.com>
2023-04-01 19:59:51 +1300

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by Andrew J Fei on 1/04/23.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
